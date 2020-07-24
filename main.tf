edffb8ed400982ac5749de6200655efbadda78ba
Sriteja Kandimalla <Sriteja.kandimalla@resmed.com>
2020-07-23 23:00:58 -0500

    adding code to create s3 bucket

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 0.12.6"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Sentinel Provider
+provider "tfe" {
+  version  = "~> 0.11.0"
+  hostname = var.tfe_host_name
+  token    = var.tfe_org_token
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = var.bucket_acl
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = var.kms_key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    name             = "sriteja"
+    Owner            = "sritej@amazon.com"
+  }
+}
+
+
+# Consuming the Sentinel module for Core baseline
+module "sentinal" {
+  source             = "tfe.tlzdemo.net/TLZ-Demo/sentinal/tfe"
+  version            = "~> 2.0.0"
+  tfe_organization   = "TLZ-Demo"
+  tfe_workspace      = "S3"
+  tfe_policies_path  = "/"
+  tfe_vcs_identifier = "RDS/terraform-tfe-sentinal"
+  tfe_oauth_token_id = var.tfe_vcs_oauth_id
+}
+
+
+output "sse_algorithm" {
+  value = aws_s3_bucket.bucket.server_side_encryption_configuration[0].rule[0].apply_server_side_encryption_by_default[0].sse_algorithm
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+# s3 variables
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket to create"
+}
+
+variable "bucket_acl" {
+  description = "ACL for S3 bucket: private, public-read, public-read-write, etc"
+  default     = "private"
+}
+
+variable "kms_key_arn" {
+  description = "ARN of the KMS key that encrypts the bucket"
+}
+
+# Sentinel variables
+
+variable "tfe_vcs_oauth_id" {
+  type        = string
+  description = "VCS Repo token id needed to associate policy set to workspace"
+}
+
+variable "tfe_host_name" {
+  description = "host_name for ptfe"
+  default     = "tfe.tlzdemo.net"
+}
+variable "tfe_org_token" {
+  type        = string
+  description = "ORG token for constructing sentinel policies and associating to workspace"
+}
